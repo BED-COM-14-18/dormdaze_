30dd2edf3fdd56ae6f5595be97c4aa05804618ee
LIMBANICHIPENI <bed-com-13-21@unima.ac.mw>
2024-10-23 10:30:14 +0100

    adding a branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
