f550fd74fbdbd2f4ed31fbd5a8ae382a6120ef35
LIMBANICHIPENI <bed-com-13-21@unima.ac.mw>
2024-11-23 14:45:12 +0100

    updated files

--- src/routes/studentOwnerDashboard/studentAuth/Auth.js
+++ src/routes/studentOwnerDashboard/studentAuth/Auth.js
@@ -103,7 +103,7 @@ function Sign() {
 
           <button
             type="submit"
-            className="w-full bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
           ><Link to={'/listingPreview'}>Sign In</Link>
        
           </button>
