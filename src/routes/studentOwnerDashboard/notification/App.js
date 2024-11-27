5d0fcb7eae5f54934f63b79d4626faad6aae7dc6
BED-COM-14-18 <bed-com-14-18@unima.ac.mw>
2024-11-27 07:08:38 +0200

    mult_task commit

--- src/routes/studentOwnerDashboard/notification/App.js
+++ src/routes/studentOwnerDashboard/notification/App.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import NotificationMobileHomePage from './notificationMobileComonent';
-import NotificationDesktopHomePage from './notificationDesktopComponent';
+// import React from 'react';
+// import NotificationMobileHomePage from './notificationMobileComonent';
+// import NotificationDesktopHomePage from './notificationDesktopComponent';
 
-const viewportContext = React.createContext({});
+// const viewportContext = React.createContext({});
 
-const ViewportProvider = ({ children }) => {
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const [height, setHeight] = React.useState(window.innerHeight);
-  const handlewindowResize = () => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-  };
+// const ViewportProvider = ({ children }) => {
+//   const [width, setWidth] = React.useState(window.innerWidth);
+//   const [height, setHeight] = React.useState(window.innerHeight);
+//   const handlewindowResize = () => {
+//     setWidth(window.innerWidth);
+//     setHeight(window.innerHeight);
+//   };
 
-  React.useEffect(() => {
-    window.addEventListener("resize", handlewindowResize);
-    return () => window.removeEventListener("resize", handlewindowResize);
-  }, []);
+//   React.useEffect(() => {
+//     window.addEventListener("resize", handlewindowResize);
+//     return () => window.removeEventListener("resize", handlewindowResize);
+//   }, []);
 
-  return (
-    <viewportContext.Provider value={{ width, height }}>
-      {children}
-    </viewportContext.Provider>
-  );
-};
+//   return (
+//     <viewportContext.Provider value={{ width, height }}>
+//       {children}
+//     </viewportContext.Provider>
+//   );
+// };
 
-const useViewport = () => {
-  const { width, height } = React.useContext(viewportContext);
-  return { width, height };
-};
+// const useViewport = () => {
+//   const { width, height } = React.useContext(viewportContext);
+//   return { width, height };
+// };
 
-/*const MobileComponent = () => <p>"Hmmm... Why is your screen so small?"</p>;
-const DesktopComponent = () => <p>"Wow, your screen is big!"</p>;*/
+// /*const MobileComponent = () => <p>"Hmmm... Why is your screen so small?"</p>;
+// const DesktopComponent = () => <p>"Wow, your screen is big!"</p>;*/
 
-const MyComponent = () => {
-  const { width } = useViewport();
-  const breakpoint = 600;
+// const MyComponent = () => {
+//   const { width } = useViewport();
+//   const breakpoint = 600;
 
-  return width < breakpoint ? <NotificationMobileHomePage /> : <NotificationDesktopHomePage />;
-};
+//   return width < breakpoint ? <NotificationMobileHomePage /> : <NotificationDesktopHomePage />;
+// };
 
-export default function Notification() {
-  return (
-    <ViewportProvider>
-      <MyComponent />
-    </ViewportProvider>
-  );
-}
+// export default function Notification() {
+//   return (
+//     <ViewportProvider>
+//       <MyComponent />
+//     </ViewportProvider>
+//   );
+// }
 
